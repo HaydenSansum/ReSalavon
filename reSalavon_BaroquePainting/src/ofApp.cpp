826bd4fad79b0bef43dbe7a499739861f66f6df9
Hayden Sansum <hayden.e.sansum@gmail.com>
2020-11-02 17:25:09 -0500

    Added the base function for storing and converting between the different types fo color classes
    This will enable a looping approach through pixels of an image and storing counts of colors as a 1D array

--- reSalavon_BaroquePainting/src/ofApp.cpp
+++ reSalavon_BaroquePainting/src/ofApp.cpp
@@ -3,6 +3,15 @@
 //--------------------------------------------------------------
 void ofApp::setup(){
 
+    // Base path to images
+    base_path = "london_2018";
+    dir.listDir(base_path);
+    
+    // Iterate through each file in the directory
+    for (int i = 0; i < 1; i++) {
+        cout << dir.getPath(i) << endl;
+    }
+        
 }
 
 //--------------------------------------------------------------
@@ -13,6 +22,82 @@ void ofApp::update(){
 //--------------------------------------------------------------
 void ofApp::draw(){
 
+}
+
+//--------------------------------------------------------------
+void ofApp::calc_top_n_colors(){
+
+}
+
+//--------------------------------------------------------------
+ofVec3f ofApp::array_index_to_bucket(int index){
+
+    int blue = index % color_channels;
+    
+    int remaining_rg = index - blue;
+    int remaining_g = remaining_rg % (color_channels * color_channels);
+    int remaining_r = remaining_rg - remaining_g;
+    
+    int red = remaining_r / (color_channels * color_channels);
+    int green = remaining_g / color_channels;
+    
+    ofVec3f bucket = ofVec3f(red, green, blue);
+    return bucket;
+}
+
+//--------------------------------------------------------------
+int ofApp::bucket_to_array_index(ofVec3f bucket){
+    
+    int red = bucket[0];
+    int green = bucket[1];
+    int blue = bucket[2];
+    
+    int r_index = red * (color_channels * color_channels);
+    int g_index = green * (color_channels);
+    int b_index = blue;
+    
+    int index = r_index + g_index + b_index;
+    return index;
+    
+}
+
+//--------------------------------------------------------------
+ofVec3f ofApp::color_to_bucket(ofColor color){
+    
+    int red = color.r;
+    int green = color.g;
+    int blue = color.b;
+    
+    int r_bucket = red / color_resolution;
+    int g_bucket = green / color_resolution;
+    int b_bucket = blue / color_resolution;
+    
+    // Account for uneven division - group extra colors at top of range
+    if(r_bucket > color_channels - 1) {
+        r_bucket = color_channels - 1;
+    }
+    if(g_bucket > color_channels - 1) {
+        g_bucket = color_channels - 1;
+    }
+    if(b_bucket > color_channels - 1) {
+        b_bucket = color_channels - 1;
+    }
+    
+    ofVec3f bucket = ofVec3f(r_bucket, g_bucket, b_bucket);
+    return bucket;
+    
+}
+
+//--------------------------------------------------------------
+ofColor ofApp::bucket_to_color(ofVec3f bucket){
+    
+    int r_bucket = bucket[0];
+    int g_bucket = bucket[1];
+    int b_bucket = bucket[2];
+    
+    ofColor color = ofColor(r_bucket*color_resolution, g_bucket*color_resolution, b_bucket*color_resolution);
+    return color;
+    
 }
 
 //--------------------------------------------------------------
--- reSalavon_BaroquePainting/src/ofApp.h
+++ reSalavon_BaroquePainting/src/ofApp.h
@@ -8,6 +8,12 @@ class ofApp : public ofBaseApp{
 		void setup();
 		void update();
 		void draw();
+    
+        void calc_top_n_colors();
+        ofVec3f array_index_to_bucket(int index);
+        int bucket_to_array_index(ofVec3f bucket);
+        ofVec3f color_to_bucket(ofColor color);
+        ofColor bucket_to_color(ofVec3f bucket);
 
 		void keyPressed(int key);
 		void keyReleased(int key);
@@ -20,5 +26,15 @@ class ofApp : public ofBaseApp{
 		void windowResized(int w, int h);
 		void dragEvent(ofDragInfo dragInfo);
 		void gotMessage(ofMessage msg);
+    
+    // FIXED VARIABLES - USER CHOICE
+    int color_resolution = 5;
+    int color_channels = 51; // Must be: floor(256 / color_resolution)
+    int color_counts [132651]; // Must be: floor(256 / color_resolution) ^ 3
+    
+    // File path
+    string base_path;
+    ofDirectory dir;
+
 		
 };
